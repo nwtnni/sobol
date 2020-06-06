84067f5a0725f2a08e66aae3fcb45e1e825286c2
Newton Ni <nwtnni@gmail.com>
2020-06-06 17:35:15 -0400

    Implement direction number recurrence
    Follows the math outlined in the paper by Bratley and Fox. There appears
    to be an error with the primitive polynomial used to generate the table:
    in the paper, it says `x^3 + x + 1`, but the polynomial that matches the
    data shown is `x^3 + x^2 + 1`.
    
    There's also a minor issue with the definition of `a_j` by Joe and Kuo,
    in that the digits seem to be reversed. They define `a_j` as:
    
    > a_j := (a_{1, j}, a_{2, j}, ..., a_{s_j - 1, j})_2
    
    But the following examples, and the coefficients used by Bratley and
    Fox, indicate that the digits are reversed.

--- sobol.py
+++ sobol.py
@@ -1,7 +1,43 @@
-import sys
+import copy
+from functools import reduce
 
 
 class Sobol:
+
+    # Compute the first `n` direction numbers `m_1, ..., m_n` for `dimension`
+    def directions(self, dimension, n):
+        if dimension == 0:
+            return [1 for _ in range(n)]
+
+        s = self.s[dimension - 1]
+        a = self.a[dimension - 1]
+        m = copy.deepcopy(self.m_i[dimension - 1])
+
+        # Compute `a XOR b`
+        def xor(a, b):
+            return a ^ b
+
+        # Compute the 1-indexed `n`th bit of integer `a`
+        def bit(a, n):
+            return (a >> (n - 1)) & 1
+
+        while len(m) < n:
+            # m_i = 2^1 * a_1 * m_{i - 1}
+            #     ^ 2^2 * a_2 * m_{i - 2}
+            #     ^ ...
+            #     ^ 2^{s - 1} * a_{s - 1} * m_{i - d + 1}
+            m_i = reduce(xor, [bit(a, d) * m[-d] << d for d in range(1, s)], 0)
+
+            #     ^ 2^s * m_{i - d}
+            m_i ^= m[-s] << s
+
+            #     ^ m_{i - d}
+            m_i ^= m[-s]
+
+            m.append(m_i)
+
+        return m
+
     # Load a series of primitive polynomials and direction numbers.
     #
     # Requires input in the following format:
@@ -15,10 +51,11 @@ class Sobol:
     # .    .    .    .
     # d_n  s_n  a_n  m_i_n_0 m_i_n_1 m_i_n_2 ...
     # ```
-    def __init__(self, path):
-        self.s = []
-        self.a = []
-        self.m_i = []
+    @staticmethod
+    def load(path):
+        s = []
+        a = []
+        m_i = []
 
         with open(path, "r") as file:
 
@@ -27,11 +64,28 @@ class Sobol:
 
             for line in file:
                 _, _s, _a, *_m_i = [int(i) for i in line.strip().split()]
-                self.s.append(_s)
-                self.a.append(_a)
-                self.m_i.append(_m_i)
+                s.append(_s)
+                a.append(_a)
+                m_i.append(_m_i)
+
+        return Sobol(s, a, m_i)
+
+    def __init__(self, s, a, m_i):
+        self.s = s
+        self.a = a
+        self.m_i = m_i
 
 
 if __name__ == "__main__":
-    path = sys.argv[1]
-    sobol = Sobol(path)
+    # Test case from Bratley, Fox
+    #
+    # Polynomial: x^3 + x^2 + 1
+    # Initial values: m_1 = 1, m_2 = 3, m_3 = 7
+    #
+    # Using notation from Joe, Kuo, we have:
+    #
+    # s = 3
+    # a = 0b010
+    # m_i = 1, 3, 7
+    sobol = Sobol([3], [2], [[1, 3, 7]])
+    assert(sobol.directions(1, 6) == [1, 3, 7, 5, 7, 43])
